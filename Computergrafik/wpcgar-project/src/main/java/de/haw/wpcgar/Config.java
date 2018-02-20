05a63ba02c55c7d85bbcb3c02802978306e5a1c6
adrian-helberg <adrianbostelmann@gmx.de>
2018-02-20 19:21:22 +0100

    Generator config

--- Computergrafik/wpcgar-project/src/main/java/de/haw/wpcgar/Config.java
+++ Computergrafik/wpcgar-project/src/main/java/de/haw/wpcgar/Config.java
@@ -6,8 +6,22 @@ package de.haw.wpcgar;
  * @author Adrian Helberg
  */
 public class Config {
+    // Distance of view
+    private static double ZOOM = 1.0;
+    private static int WIDTH = 800;
+    private static int HEIGHT = 800;
+    private static double X = 0.0;
+    private static double Y = 0.0;
 
-    public Config() {
+    // Window dimensions
+    public final static int WINDOW_WIDTH = 800;
+    public final static int WINDOW_HEIGHT = 800;
 
-    }
+    // Density through height map
+    private static boolean SHOW_DENSITY = false;
+    private static String DENSITY_NAME = "heightmap";
+
+    // Seed
+    private static boolean USE_DEFAULT_SEED = true;
+    private static String DEFAULT_SEED = "";
 }
